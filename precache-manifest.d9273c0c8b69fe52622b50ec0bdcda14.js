a1a66ee0a36379b534c82616cb319f1f52df517e
Abdul <abdul.rm.jahangir@gmail.com>
2020-05-22 01:21:13 -0500

    Updates

---
+++ precache-manifest.d9273c0c8b69fe52622b50ec0bdcda14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800c60ace77e3a2aa373b8c9f176782e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21a14dfd287e8245a58",
+    "url": "/static/css/main.1beef5ff.chunk.css"
+  },
+  {
+    "revision": "bbca3bcceab6eada7a0d",
+    "url": "/static/js/2.75899ee3.chunk.js"
+  },
+  {
+    "revision": "b21a14dfd287e8245a58",
+    "url": "/static/js/main.84b8ee14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
